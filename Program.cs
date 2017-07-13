286772e601d5b03544510cdfab8fc94ba15dab48
Esheel <esheelroy@gmail.com>
2017-07-13 16:20:55 +1200

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Keep it G rated!");
+            Console.Clear();
+            
+        }
+    }
+}
